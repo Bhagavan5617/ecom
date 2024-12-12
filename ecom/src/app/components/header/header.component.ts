a1571cc3f012cf3cda1673ba242815d7a6bde03d
bhagavan5617 <bhagavanpabbu23@gmail.com>
2024-12-12 09:59:53 +0530

    chnages

--- ecom/src/app/components/header/header.component.ts
+++ ecom/src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  isFixed:boolean =false;
+  @HostListener('window:scroll',[])
+  onWindowScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    this.isFixed = scrollPosition > 100; // Apply 'fixed-position' when scrolling past 50px
+  }
 }
